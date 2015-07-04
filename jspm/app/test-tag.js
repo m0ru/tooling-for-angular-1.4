d8f34a539b27c64605312ed21c431535e8c477cf
moru <kevin.singer@student.tuwien.ac.at>
2015-07-04 20:31:47 +0200

    ES6 imports working.

--- jspm/app/test-tag.js
+++ jspm/app/test-tag.js
@@ -1,13 +1,3 @@
-/*
-var factoryFun = function($log){
-        $log.info('test-tag loading.');
-        return {
-            template: '<p> le directive haz loaded!</p>'
-        };
-}
-factoryFun.$inject = ['$log'];
-module.exports = factoryFun;*/
-
 function testTag($log){
         $log.info('test-tag loading.');
         return {
