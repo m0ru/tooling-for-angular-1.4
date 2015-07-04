8945021169d0c756b166942e49765f6950667920
moru <kevin.singer@student.tuwien.ac.at>
2015-07-04 20:08:28 +0200

    Testing ES6 imports. disfunct atm.

--- jspm/app/app.js
+++ jspm/app/app.js
@@ -1,7 +1,7 @@
 console.log("app.js");
 
 var angular = require('angular');
-console.log(angular);
+//console.log(angular);
 
 
 /*
@@ -14,13 +14,18 @@ angular.module('myModule', [
 */
 
 
+import testTag from './test-tag';
+console.log(testTag);
+
+/*
 angular.module('myModule',[])
     .directive('testTag', require('app/test-tag'));
-    
+    */
+
 
 angular.bootstrap(document, ['myModule'], {
   // make sure dependency injection works after minification
   // see https://docs.angularjs.org/guide/production
   // and https://docs.angularjs.org/guide/di#dependency-annotation
-  strictDi: true 
+  strictDi: true
 });
--- jspm/app/test-tag.js
+++ jspm/app/test-tag.js
@@ -1,5 +1,18 @@
-module.exports = [function(){
+/*
+var factoryFun = function($log){
+        $log.info('test-tag loading.');
         return {
             template: '<p> le directive haz loaded!</p>'
         };
-}]
+}
+factoryFun.$inject = ['$log'];
+module.exports = factoryFun;*/
+
+function testTag($log){
+        $log.info('test-tag loading.');
+        return {
+            template: '<p> le directive haz loaded!</p>'
+        };
+}
+testTag.$inject = ['$log'];
+export default testTag;
